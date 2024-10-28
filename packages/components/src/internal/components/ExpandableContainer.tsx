dfde5fc9732a83c31d21054e6cc54b5ff238f503
XingY <xyang@labkey.com>
2024-10-28 13:23:08 -0700

    expand after archive

--- packages/components/src/internal/components/ExpandableContainer.tsx
+++ packages/components/src/internal/components/ExpandableContainer.tsx
@@ -2,7 +2,8 @@
  * Copyright (c) 2016-2019 LabKey Corporation. All rights reserved. No portion of this work may be reproduced in
  * any form or by any electronic or mechanical means without written permission from LabKey Corporation.
  */
-import React, { PropsWithChildren } from 'react';
+import React, { FC, memo, useCallback, PropsWithChildren, useState, useEffect, useMemo } from 'react';
+
 import classNames from 'classnames';
 
 import { SVGIcon } from './base/SVGIcon';
@@ -21,106 +22,87 @@ interface Props extends PropsWithChildren {
     useGreyTheme?: boolean;
 }
 
-interface State {
-    isHover?: boolean;
-    visible?: boolean;
-}
+export const ExpandableContainer: FC<Props> = memo(props => {
+    const { clause, containerCls, iconFaCls, iconSrc, initExpanded, isExpandable,
+        links, noIcon, onClick, rowCls = 'row', useGreyTheme, children } = props;
+    const [visible, setVisible] = useState<boolean>(false);
+    const [isHover, setIsHover] = useState<boolean>(false);
 
-export class ExpandableContainer extends React.PureComponent<Props, State> {
-    constructor(props: Props) {
-        super(props);
+    useEffect(() => {
+        setVisible(!!initExpanded);
+    }, [initExpanded]);
 
-        this.state = {
-            visible: props.initExpanded || false,
-            isHover: false,
-        };
-    }
+    const hasOnClick = useMemo(() => {
+        return onClick !== undefined;
+    }, [onClick]);
 
-    handleClick = (): void => {
-        if (!this.props.isExpandable) {
-            this.props.onClick?.(false);
+    const containerDivCls = useMemo(() => {
+        return useGreyTheme ? 'container-expandable-grey' : 'container-expandable-blue';
+    }, [useGreyTheme]);
+
+    const handleClick = useCallback(() => {
+        if (!isExpandable) {
+            onClick?.(false);
             return;
         }
 
-        this.setState(
-            state => ({
-                visible: !state.visible,
-            }),
-            () => {
-                this.props.onClick?.(this.state.visible);
-            }
-        );
-    };
+        setVisible((prevState) => {
+            onClick?.(!prevState);
+            return !prevState;
+        })
+    }, [isExpandable, onClick, setVisible]);
 
-    handleMouseEnter = (): void => {
-        this.setState(() => ({ isHover: true }));
-    };
+    const handleMouseEnter = useCallback(() => {
+        setIsHover(true);
+    }, [setIsHover]);
 
-    handleMouseLeave = (): void => {
-        this.setState(() => ({ isHover: false }));
-    };
+    const handleMouseLeave = useCallback(() => {
+        setIsHover(false);
+    }, [setIsHover]);
 
-    render() {
-        const {
-            children,
-            rowCls = 'row',
-            noIcon,
-            iconSrc,
-            iconFaCls,
-            isExpandable,
-            clause,
-            links,
-            containerCls,
-            useGreyTheme,
-        } = this.props;
-        const { visible, isHover } = this.state;
-        const hasOnClick = this.props.onClick !== undefined;
-        const containerDivCls = useGreyTheme ? 'container-expandable-grey' : 'container-expandable-blue';
-
-        return (
-            <div className={classNames(rowCls, 'container-expandable', { disabled: !isExpandable })}>
+    return (
+        <div className={classNames(rowCls, 'container-expandable', {disabled: !isExpandable})}>
+            <div
+                onClick={hasOnClick || isExpandable ? handleClick : undefined}
+                onMouseEnter={isExpandable ? handleMouseEnter : undefined}
+                onMouseLeave={isExpandable ? handleMouseLeave : undefined}
+                className={classNames(
+                    containerCls,
+                    containerDivCls,
+                    {'container-expandable-child__inactive': visible},
+                    {'container-expandable__active': isHover || visible},
+                    {'container-expandable__inactive': !isHover && !visible}
+                )}
+            >
+                {!noIcon && (
+                    <i className="container-expandable-child__img">
+                        {iconFaCls ? (
+                            <i style={{padding: '5px'}} className={'fa fa-' + iconFaCls}/>
+                        ) : (
+                            <SVGIcon iconSrc={iconSrc} isActive={isHover} height="50px" width="50px"/>
+                        )}
+                    </i>
+                )}
                 <div
-                    onClick={hasOnClick || isExpandable ? this.handleClick : undefined}
-                    onMouseEnter={isExpandable ? this.handleMouseEnter : undefined}
-                    onMouseLeave={isExpandable ? this.handleMouseLeave : undefined}
-                    className={classNames(
-                        containerCls,
-                        containerDivCls,
-                        { 'container-expandable-child__inactive': visible },
-                        { 'container-expandable__active': isHover || visible },
-                        { 'container-expandable__inactive': !isHover && !visible }
-                    )}
+                    onClick={hasOnClick || isExpandable ? handleClick : undefined}
+                    className={classNames('pull-right', 'container-expandable-child__chevron', {
+                        'text-muted': !isExpandable,
+                    })}
                 >
-                    {!noIcon && (
-                        <i className="container-expandable-child__img">
-                            {iconFaCls ? (
-                                <i style={{ padding: '5px' }} className={'fa fa-' + iconFaCls} />
-                            ) : (
-                                <SVGIcon iconSrc={iconSrc} isActive={isHover} height="50px" width="50px" />
-                            )}
-                        </i>
-                    )}
-                    <div
-                        onClick={hasOnClick || isExpandable ? this.handleClick : undefined}
-                        className={classNames('pull-right', 'container-expandable-child__chevron', {
-                            'text-muted': !isExpandable,
+                    <i
+                        onClick={hasOnClick || isExpandable ? handleClick : undefined}
+                        className={classNames('fa', {
+                            'fa-chevron-down': visible,
+                            'fa-chevron-right': !visible,
                         })}
-                    >
-                        <i
-                            onClick={hasOnClick || isExpandable ? this.handleClick : undefined}
-                            className={classNames('fa', {
-                                'fa-chevron-down': visible,
-                                'fa-chevron-right': !visible,
-                            })}
-                        />
-                    </div>
-                    <div className="container-expandable-heading">
-                        {clause}
-                        {links}
-                    </div>
+                    />
                 </div>
-                {visible && children}
+                <div className="container-expandable-heading">
+                    {clause}
+                    {links}
+                </div>
             </div>
-        );
-    }
-}
+            {visible && children}
+        </div>
+    )
+});
