b32355a87e767b17c98cc6fe1ab3cf321bbbaf07
alanv <alanv@labkey.com>
2024-10-30 12:45:21 -0500

    Delete build-test package

--- packages/build-test/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Comp } from './Comp';
-import { thing } from './utils';
-
-
-
-export default {
-    thing,
-    Comp,
-}
--- packages/build-test/src/utils.ts
+++
@@ -1,5 +0,0 @@
-
-export function thing (num: number[]) {
-    // Should not compile because props.nums.at is ES2022, and our build lib is ES2021
-    console.log(num.at(-1));
-}
