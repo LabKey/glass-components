d34b988d52e621a4c9e67888755784d2b4ccc0ea
alanv <alanv@labkey.com>
2024-12-04 17:36:36 -0600

    FileAttachmentEntry: convert to FC, simplify props

--- packages/components/src/internal/components/files/FileAttachmentEntry.tsx
+++ packages/components/src/internal/components/files/FileAttachmentEntry.tsx
@@ -1,41 +1,28 @@
-import React, { PureComponent } from 'react';
+import React, { FC, memo, useCallback } from 'react';
 
 interface Props {
-    allowDelete?: boolean;
-    deleteTitleText?: string;
     downloadUrl?: string;
     name: string;
     onDelete?: (name: string) => void;
 }
 
-export class FileAttachmentEntry extends PureComponent<Props, any> {
-    static defaultProps = {
-        allowDelete: true,
-        deleteTitleText: 'Remove file',
-    };
-
-    render() {
-        const { allowDelete, downloadUrl, onDelete, name, deleteTitleText } = this.props;
-        return (
-            <div key={name} className="attached-file--container">
-                {allowDelete && (
-                    <span
-                        className="fa fa-times-circle file-upload__remove--icon"
-                        onClick={() => onDelete(name)}
-                        title={deleteTitleText}
-                    />
-                )}
-                <span className="fa fa-file-text attached-file--icon attached-file--bottom-padding" />
-                {downloadUrl ? (
-                    <strong>
-                        <a href={downloadUrl} title={name}>
-                            <div className="file-listing-filename">{name}</div>
-                        </a>
-                    </strong>
-                ) : (
-                    name
-                )}
-            </div>
-        );
-    }
-}
+export const FileAttachmentEntry: FC<Props> = memo(props => {
+    const { downloadUrl, onDelete, name } = props;
+    const onClick = useCallback(() => onDelete(name), [onDelete, name]);
+    const deleteIconClassName = 'fa fa-times-circle file-upload__remove--icon';
+    return (
+        <div className="attached-file--container">
+            {onDelete && <span className={deleteIconClassName} onClick={onClick} title="Remove file" />}
+            <span className="fa fa-file-text attached-file--icon" />
+            {downloadUrl && (
+                <strong>
+                    <a href={downloadUrl} title={name}>
+                        <div className="file-listing-filename">{name}</div>
+                    </a>
+                </strong>
+            )}
+            {!downloadUrl && <>{name}</>}
+        </div>
+    );
+});
+FileAttachmentEntry.displayName = 'FileAttachmentEntry';
