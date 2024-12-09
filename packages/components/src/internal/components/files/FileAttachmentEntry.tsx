17bf7c97d7a40c7c590b9bc904a33ed58de17f63
alanv <alanv@labkey.com>
2024-12-09 15:28:05 -0600

    Remove unused styles, align naming conventions

--- packages/components/src/internal/components/files/FileAttachmentEntry.tsx
+++ packages/components/src/internal/components/files/FileAttachmentEntry.tsx
@@ -9,15 +9,15 @@ interface Props {
 export const FileAttachmentEntry: FC<Props> = memo(props => {
     const { downloadUrl, onDelete, name } = props;
     const onClick = useCallback(() => onDelete(name), [onDelete, name]);
-    const deleteIconClassName = 'fa fa-times-circle file-upload__remove--icon';
+    const deleteIconClassName = 'fa fa-times-circle attached-file__remove-icon';
     return (
-        <div className="attached-file--container">
+        <div className="attached-file__container">
             {onDelete && <span className={deleteIconClassName} onClick={onClick} title="Remove file" />}
-            <span className="fa fa-file-text attached-file--icon" />
+            <span className="fa fa-file-text attached-file__icon" />
             {downloadUrl && (
                 <strong>
                     <a href={downloadUrl} title={name}>
-                        <div className="file-listing-filename">{name}</div>
+                        <div className="attached-file__filename">{name}</div>
                     </a>
                 </strong>
             )}
