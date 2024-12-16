fa55e1b14ecc82aee5a0f5fd007b2bdac64ba2dd
alanv <alanv@labkey.com>
2024-12-16 11:33:41 -0600

    request: Add RequesstHandler

--- packages/components/src/internal/request.ts
+++ packages/components/src/internal/request.ts
@@ -3,15 +3,29 @@ import { Ajax, Utils, RequestOptions } from '@labkey/api';
 import { handleRequestFailure } from './util/utils';
 
 type Options = Omit<RequestOptions, 'success' | 'failure'>;
+type RequestHandler = (request: XMLHttpRequest) => void;
 
-export function request<T>(options: Options, errorLogMsg = 'Error making ajax request'): Promise<T> {
+/**
+ * This is a light wrapper around Ajax.request that takes the same options, minus success and failure. Instead of
+ * passing success or failure you wrap the call in try/catch and await the call to request e.g.:
+ *
+ * try {
+ *     const resp = await request(myOptions);
+ * } catch (error) {
+ *     // handle error here
+ * }
+ */
+export function request<T>(
+    options: Options,
+    errorLogMsg = 'Error making ajax request',
+    requestHandler?: RequestHandler
+): Promise<T> {
     return new Promise((resolve, reject) => {
-        Ajax.request({
+        const xmlHttpRequest = Ajax.request({
             ...options,
-            success: Utils.getCallbackWrapper((res: T) => {
-                resolve(res);
-            }),
+            success: Utils.getCallbackWrapper((res: T) => resolve(res)),
             failure: handleRequestFailure(reject, errorLogMsg),
         });
+        requestHandler?.(xmlHttpRequest);
     });
 }
