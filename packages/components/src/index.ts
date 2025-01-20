db960b07975a305631a5a1efbc43e1f331981592
Alan Vezina <alanv@labkey.com>
2025-01-20 13:11:56 -0600

    Fix Issue 51839 (#1679)
    * Export useLoadableState
    
    * Add useModalState
    
    * Update release notes
    
    * Prep for release

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -42,7 +42,6 @@ import { isLoading, LoadingState } from './public/LoadingState';
 import { ExtendedMap } from './public/ExtendedMap';
 import { useContainerUser } from './internal/components/container/actions';
 import { request } from './internal/request';
-import { useLoadableState } from './internal/useLoadableState';
 
 import {
     ServerContextConsumer,
@@ -861,7 +860,7 @@ import { Discussions } from './internal/announcements/Discussions';
 import { Thread } from './internal/announcements/Thread';
 import { ThreadBlock } from './internal/announcements/ThreadBlock';
 import { ThreadEditor } from './internal/announcements/ThreadEditor';
-import { useNotAuthorized, useNotFound, usePortalRef, useTimeout } from './internal/hooks';
+import { useModalState, useNotAuthorized, useNotFound, usePortalRef, useTimeout } from './internal/hooks';
 import {
     TEST_BIO_LIMS_ENTERPRISE_MODULE_CONTEXT,
     TEST_BIO_LIMS_STARTER_MODULE_CONTEXT,
@@ -911,6 +910,7 @@ import { CheckboxLK } from './internal/Checkbox';
 import { ArchivedFolderTag } from './internal/components/folder/ArchivedFolderTag';
 import { FilterCriteriaRenderer } from './internal/FilterCriteriaRenderer';
 import { getQueryTestAPIWrapper } from './internal/query/APIWrapper';
+import { useLoadableState } from './internal/useLoadableState';
 
 // See Immer docs for why we do this: https://immerjs.github.io/immer/docs/installation#pick-your-immer-version
 enableMapSet();
@@ -1665,7 +1665,6 @@ export {
     DATE_FORMATS_TOPIC,
     request,
     useRequestHandler,
-    useLoadableState,
     // devTools functions
     applyDevTools,
     devToolsActive,
@@ -1850,6 +1849,8 @@ export {
     // Custom labels
     getModuleCustomLabels,
     FilterCriteriaRenderer,
+    useLoadableState,
+    useModalState,
 };
 
 //  Due to babel-loader & typescript babel plugins we need to export/import types separately. The babel plugins require
